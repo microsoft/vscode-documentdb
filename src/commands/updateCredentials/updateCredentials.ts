9993a9f213c33507ae4e7108bb78c1453105437f
Tomasz Naumowicz <tnaumowicz@microsoft.com>
2025-05-05 17:21:19 +0200

    fix: closing connection when credentials are changed

--- src/commands/updateCredentials/updateCredentials.ts
+++ src/commands/updateCredentials/updateCredentials.ts
@@ -6,6 +6,8 @@
 import { AzureWizard, nonNullValue, type IActionContext } from '@microsoft/vscode-azext-utils';
 import * as l10n from '@vscode/l10n';
 import ConnectionString from 'mongodb-connection-string-url';
+import { ClustersClient } from '../../documentdb/ClustersClient';
+import { CredentialCache } from '../../documentdb/CredentialCache';
 import { Views } from '../../documentdb/Views';
 import { type DocumentDBClusterItem } from '../../tree/connections-view/DocumentDBClusterItem';
 import { refreshView } from '../refreshView/refreshView';
@@ -52,6 +54,15 @@ export async function updateCredentials(context: IActionContext, node?: Document
      * The better solution would be, in general, to not cache the connection string at all.
      * And only access it from the storage when needed. This would be a better and more
      * secure solution.
+     *
+     * On top, the existing connection should be closed and a new one should be created.
+     * Imagine that the username is changed so the permissions would change but noone notices.
+     * That's why the connection has to be closed and a new one created.
+     * And what about tabs opened with the old connection? They should be closed too.
+     * This is a bigger change and should be done in a separate PR.
+     * So for now, we just refresh the view to make sure the new credentials are used.
      */
+    await ClustersClient.deleteClient(node.cluster.id);
+    CredentialCache.deleteCredentials(node.cluster.id);
     await refreshView(context, Views.ConnectionsView);
 }
