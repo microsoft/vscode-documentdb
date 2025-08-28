d9c44fe153588598ec711313cddf34211fc66139
Tomasz Naumowicz <tnaumowicz@microsoft.com>
2025-08-28 16:44:01 +0200

    wip: placeholder for extended cluster info for RU

--- src/tree/azure-resources-view/mongo-ru/RUBranchDataProvider.ts
+++ src/tree/azure-resources-view/mongo-ru/RUBranchDataProvider.ts
@@ -5,15 +5,22 @@
 
 import * as vscode from 'vscode';
 
+import { getResourceGroupFromId, uiUtils } from '@microsoft/vscode-azext-azureutils';
 import {
     callWithTelemetryAndErrorHandling,
     createContextValue,
     type IActionContext,
 } from '@microsoft/vscode-azext-utils';
-import { type AzureResource, type BranchDataProvider } from '@microsoft/vscode-azureresources-api';
-import { MongoClustersExperience } from '../../../DocumentDBExperiences';
+import {
+    type AzureResource,
+    type AzureSubscription,
+    type BranchDataProvider,
+} from '@microsoft/vscode-azureresources-api';
+import { MongoClustersExperience, MongoExperience } from '../../../DocumentDBExperiences';
 import { Views } from '../../../documentdb/Views';
 import { ext } from '../../../extensionVariables';
+import { createCosmosDBManagementClient } from '../../../utils/azureClients';
+import { nonNullProp } from '../../../utils/nonNull';
 import { type ExtendedTreeDataProvider } from '../../ExtendedTreeDataProvider';
 import { type TreeElement } from '../../TreeElement';
 import { isTreeElementWithContextValue, type TreeElementWithContextValue } from '../../TreeElementWithContextValue';
@@ -31,6 +38,11 @@ export class RUBranchDataProvider
     extends vscode.Disposable
     implements BranchDataProvider<AzureResource, TreeElement>, ExtendedTreeDataProvider<TreeElement>
 {
+    // these three properties support lazy detail information loading
+    private detailsCacheUpdateRequested = false; // this is just a "wip" and low prio disabled for this iteration
+    private detailsCache: Map<string, ClusterModel> = new Map<string, ClusterModel>();
+    private itemsToUpdateInfo: Map<string, RUResourceItem> = new Map<string, RUResourceItem>();
+
     /**
      * Cache for tracking parent-child relationships to support the getParent method.
      */
@@ -226,14 +238,51 @@ export class RUBranchDataProvider
     }
 
     getResourceItem(resource: AzureResource): TreeElement | Thenable<TreeElement> {
-        const clusterInfo: ClusterModel = {
-            ...resource,
-            dbExperience: MongoClustersExperience,
-        } as ClusterModel;
+        return callWithTelemetryAndErrorHandling('getResourceItem', async (context: IActionContext) => {
+            context.telemetry.properties.view = Views.AzureResourcesView;
+            context.telemetry.properties.branch = 'ru';
 
-        const clusterItem = new RUResourceItem(resource.subscription, clusterInfo);
+            if (this.detailsCacheUpdateRequested) {
+                void this.updateMetadataCache(context, resource.subscription, 1000 * 60 * 5).then(() => {
+                    /**
+                     * Instances of MongoClusterItem were  stored in the itemsToUpdateInfo map,
+                     * so that when the cache is updated, the items can be refreshed.
+                     * I had to keep all of them in the map becasuse refresh requires the actual MongoClusterItem instance.
+                     */
+                    this.itemsToUpdateInfo.forEach((value: RUResourceItem) => {
+                        value.cluster = {
+                            ...value.cluster,
+                            ...this.detailsCache.get(value.cluster.id),
+                        };
+                        this.refresh(value);
+                    });
 
-        return clusterItem;
+                    this.itemsToUpdateInfo.clear();
+                });
+            }
+
+            let clusterInfo: ClusterModel = {
+                ...resource,
+                dbExperience: MongoClustersExperience,
+            } as ClusterModel;
+
+            // 2. lookup the details in the cache, on subsequent refreshes, the details will be available in the cache
+            if (this.detailsCache.has(clusterInfo.id)) {
+                clusterInfo = {
+                    ...clusterInfo,
+                    ...this.detailsCache.get(clusterInfo.id),
+                };
+            }
+
+            const clusterItem = new RUResourceItem(resource.subscription, clusterInfo);
+
+            // 3. store the item in the update queue, so that when the cache is updated, the item can be refreshed
+            this.itemsToUpdateInfo.set(clusterItem.id, clusterItem);
+
+            ext.state.wrapItemInStateHandling(clusterItem, () => this.refresh(clusterItem));
+
+            return clusterItem;
+        }) as TreeElement | Thenable<TreeElement>; // Cast to ensure correct type;
     }
 
     async getTreeItem(element: TreeElement): Promise<vscode.TreeItem> {
@@ -254,5 +303,80 @@ export class RUBranchDataProvider
         }
 
         treeItem.contextValue = createContextValue(contextValues);
+    }
+
+    async updateMetadataCache(
+        _context: IActionContext,
+        subscription: AzureSubscription,
+        cacheDuration: number,
+    ): Promise<void> {
+        return callWithTelemetryAndErrorHandling(
+            'getResourceItem.updateMetadataCache',
+            async (context: IActionContext) => {
+                context.telemetry.properties.view = Views.AzureResourcesView;
+                context.telemetry.properties.branch = 'documentdb';
+
+                try {
+                    console.debug(
+                        'Loading metadata cache for %s/%s',
+                        context.telemetry.properties.view,
+                        context.telemetry.properties.branch,
+                    );
+
+                    this.detailsCacheUpdateRequested = false;
+
+                    setTimeout(() => {
+                        this.detailsCache.clear();
+                        this.detailsCacheUpdateRequested = true;
+                        console.debug(
+                            'Cleared metadata cache for %s/%s',
+                            context.telemetry.properties.view,
+                            context.telemetry.properties.branch,
+                        );
+                    }, cacheDuration); // clear cache when the it expires
+
+                    // subscription comes from different azure packages in callers; cast here intentionally
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-argument
+                    const managementClient = await createCosmosDBManagementClient(context, subscription as any);
+
+                    let accounts = await uiUtils.listAllIterator(managementClient.databaseAccounts.list());
+                    accounts = accounts.filter((account) => account.kind === 'MongoDB'); // ignore non-ru accounts
+
+                    console.debug(
+                        'Loaded metadata for %s/%s: %d entries',
+                        context.telemetry.properties.view,
+                        context.telemetry.properties.branch,
+                        accounts.length,
+                    );
+
+                    accounts.map((mongoClusterAccount) => {
+                        this.detailsCache.set(
+                            nonNullProp(mongoClusterAccount, 'id', 'mongoClusterAccount.id', 'RUBranchDataProvider.ts'),
+                            {
+                                dbExperience: MongoExperience,
+                                id: mongoClusterAccount.id!,
+                                name: mongoClusterAccount.name!,
+                                resourceGroup: getResourceGroupFromId(mongoClusterAccount.id!),
+
+                                location: mongoClusterAccount.location,
+                                serverVersion: mongoClusterAccount?.apiProperties?.serverVersion,
+
+                                systemData: {
+                                    createdAt: mongoClusterAccount.systemData?.createdAt,
+                                },
+                            },
+                        );
+                    });
+                } catch (e) {
+                    console.debug(
+                        'Failed to load metadata for %s/%s',
+                        context.telemetry.properties.view,
+                        context.telemetry.properties.branch,
+                    );
+                    console.debug({ ...context, ...subscription });
+                    throw e;
+                }
+            },
+        );
     }
 }
