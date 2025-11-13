d32f2b0ecdff47c6851e9a3f0c24a24b53f913c7
Tomasz Naumowicz <tnaumowicz@microsoft.com>
2025-11-13 16:58:52 +0100

    feat: disable NL-2-Query (transition period)

---
+++ src/webviews/documentdb/collectionView/constants.ts
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+/**
+ * Feature flag to enable/disable AI query generation features.
+ * When false, the Generate button and AI input area are disabled.
+ */
+export const ENABLE_AI_QUERY_GENERATION = false;
